196207eec9ec1427032b6b3cc829cb13239dbed4
Umesh02 <u.teach.and.i.learn@gmail.com>
2019-10-18 17:20:37 +0530

    End of L13.  Added Form for Filtering

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomesComponent } from './homes/homes.component';
+import { HomeTypeFilterComponent } from './home-type-filter/home-type-filter.component';
 
 @NgModule({
-  declarations: [AppComponent, HomesComponent],
+  declarations: [AppComponent, HomesComponent, HomeTypeFilterComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/home-type-filter/home-type-filter.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-type-filter',
+  templateUrl: './home-type-filter.component.html'
+})
+export class HomeTypeFilterComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
