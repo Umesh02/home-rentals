40f0b4d59c03671050dde2da4d5af0e594988704
Umesh02 <u.teach.and.i.learn@gmail.com>
2019-10-18 12:26:40 +0530

    End of L6:  Added Data Service to Fetch Data

--- src/app/homes/homes.component.ts
+++ src/app/homes/homes.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-homes',
   templateUrl: './homes.component.html'
 })
 export class HomesComponent implements OnInit {
-  constructor() {}
+  homes$ = this.dataService.getHomes();
+
+  constructor(private dataService: DataService) {}
 
   ngOnInit() {}
 }
