642aa1efbaf52ddb71729cb7424e4cd7a8012a27
Umesh02 <u.teach.and.i.learn@gmail.com>
2019-10-18 18:07:19 +0530

    End of L15: Set Selected filters in URL

--- src/app/homes/homes.component.ts
+++ src/app/homes/homes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../data.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-homes',
@@ -9,7 +10,12 @@ export class HomesComponent implements OnInit {
   homeTypeDropdownOpen = false;
   homes$ = this.dataService.getHomes();
 
-  constructor(private dataService: DataService) {}
+  constructor(private dataService: DataService, private router: Router) {}
 
   ngOnInit() {}
+
+  homeTypeFilterApplied($event) {
+    this.homeTypeDropdownOpen = false;
+    this.router.navigate(['homes'], { queryParams: { 'home-type': $event } });
+  }
 }
